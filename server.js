0a983b645a32b4b9354c1dc658f796c02fd635ed
mikeorgill <org21001@byui.edu>
2023-12-06 20:48:41 -0700

    Fixed errors on server.js file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express(); //app is a backend application
 
 app.get("/",(req,res)=>{
-    RegExp.send("Welcome to my Mike's backend application");
+    res.send("Welcome to my Mike's backend application");
 });//forward slash means the context root of the application, or the url without
 
 app.get("/mike",(req,res)=>{
