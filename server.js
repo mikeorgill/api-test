4eb9bd0706af06d6d00532e21584a4a6dab6dd0d
mikeorgill <org21001@byui.edu>
2023-12-06 19:00:31 -0700

    added server.js and Dockerfile

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express(); //app is a backend application
+
+app.get("/",(req,res)=>{
+    RegExp.send("Welcome to my Mike's backend application");
+});//forward slash means the context root of the application, or the url without
+
+app.get("/mike",(req,res)=>{
+    res.send("Hello Mike!");
+});
+
+app.listen(3000,()=>{
+    console.log("Listening");
+});
